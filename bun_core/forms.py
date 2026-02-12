71281729668064ab73de3138f1dee51c6cc5701f
Buny08dev <goldazamat08@gmail.com>
2026-02-12 09:21:16 +0300

    search func 1

--- bun_core/forms.py
+++ bun_core/forms.py
@@ -10,3 +10,22 @@ class AddProduct(forms.ModelForm):
         widgets={
             'name':forms.TextInput(attrs={"placeholder":"name"})
         }
+
+class SearchForm(forms.Form):
+    search = forms.CharField(
+        label='',
+        min_length=3,
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Search',
+            'aria-label': 'Search',
+        
+        },)
+    )
+    def clean_search(self):
+        data = self.cleaned_data['search']
+        # masalan: qidiruv matnida raqam bo‘lmasligi kerak
+        if any(char.isdigit() for char in data):
+            raise forms.ValidationError("Qidiruv matnida raqam bo‘lmasligi kerak!")
+        return data
