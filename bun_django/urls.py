8333c545b15ad2b341ba968370a78d1be3e83cf3
Buny08dev <goldazamat08@gmail.com>
2026-02-20 13:04:40 +0300

    debugToolbarni ikki marta yozib qoyganimni( uchun proyectimniga ishlashina halaqit barmidi lekin) baz- biram togirladim

--- bun_django/urls.py
+++ bun_django/urls.py
@@ -46,7 +46,6 @@ urlpatterns = [
     path("",include("bun_core.urls")),
     path("users/",include('bun_users.urls')),
     path('admin/', admin.site.urls),
-    path('__debug__/',include('debug_toolbar.urls')),
     
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
